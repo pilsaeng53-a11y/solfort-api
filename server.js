43dd72be4d2d122fd4ecfe033dbb1e27b95050a8
pilsaeng53-a11y <pilsaeng53@gmail.com>
2026-03-23 00:48:13 +0900

    server.js

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const app = express();
 
 app.use(express.json());
 
-// 기본 라우트만 먼저 연결
 const coinIconsRoute = require("./routes/coinIcons");
 const marketDataRoute = require("./routes/marketData");
 const aiSignalsRoute = require("./routes/aiSignals");
 const symbolsRoute = require("./routes/symbols");
+const userSettingsRoute = require("./routes/userSettings");
 
 app.use("/coin-icons", coinIconsRoute);
 app.use("/market-data", marketDataRoute);
 app.use("/ai-signals", aiSignalsRoute);
 app.use("/symbols", symbolsRoute);
+app.use("/user-settings", userSettingsRoute);
 
 app.get("/", (req, res) => {
   res.send("SolFort API running 🚀");
